1ea4a9fb991d889ad598cc14357f2a55cb5a46fb
unknown <yaminulhoque@ug.me.buet.ac.bd>
2020-11-16 11:23:33 +0600

    heroku6

--- backend/config.js
+++ backend/config.js
@@ -4,10 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 //access the env file
 
-const db_url = 'mongodb+srv://yamin02:Ch@ndanpura2@yamin-amazon.llwl6.mongodb.net/test' ;
 export default {
     PORT : process.env.PORT || 5000,
-    MONGODB_URL : db_url ,
+    MONGODB_URL : process.env.MONGODB_URL ,
     JWT_SECRET: process.env.JWT_SECRET ,
     PAYPAL_CLIENT_ID : process.env.PAYPAL_CLIENT_ID,
 }
