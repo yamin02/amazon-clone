d285967aa9992f48ac13256932339334edf49192
unknown <yaminulhoque@ug.me.buet.ac.bd>
2020-11-16 10:25:32 +0600

    added db

--- backend/config.js
+++ backend/config.js
@@ -4,9 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 //access the env file
 
+const db_url = 'mongodb+srv://yamin02:Ch@ndanpura2@yamin-amazon.llwl6.mongodb.net/test' ;
 export default {
     PORT : process.env.PORT || 5000,
-    MONGODB_URL : process.env.MONGODB_URL,
+    MONGODB_URL : process.env.MONGODB_URL || db_url ,
     JWT_SECRET: process.env.JWT_SECRET ,
     PAYPAL_CLIENT_ID : process.env.PAYPAL_CLIENT_ID,
 }
